1eb7c53da1d2d76bef5d8fad5db3cc4da5f1a99b
kasai <c1357176@st.kanazawa-it.ac.jp>
2023-06-24 11:20:50 +0900

    first_commit

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 
-int main(void){
-    printf("helloWorld.\n");
-
-    return 0;
+int main(void){   
+ int num;
+ printf("文字を入力してください。");
+ scanf("%c",&num);
+ printf("入力された文字は：%c\n",num);
+ return 0
 }
